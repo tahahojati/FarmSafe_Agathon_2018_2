b2236b77274ac85b4679d91bc75c2273ee7c2f7d
matthew-schultz <mschultz@hawaii.edu>
2018-01-13 21:08:48 -1000

    create database package and CourseDbSchema class

---
+++ FarmSafe/app/src/main/java/com/tpourjalali/farmsafe/database/CourseDbSchema.java
@@ -0,0 +1,11 @@
+package com.tpourjalali.farmsafe.database;
+
+/**
+ * Created by mschultz on 1/13/2018.
+ */
+
+public class CourseDbSchema {
+    public static final class CourseTable {
+        public static final String NAME = "courses";
+    }
+}
