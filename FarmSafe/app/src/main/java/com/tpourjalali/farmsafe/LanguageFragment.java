3761939a4f816599c639e4780d19411a136da2e2
Taha <taha.hojati@gmail.com>
2018-01-14 11:45:01 -1000

    set language page to first page

--- FarmSafe/app/src/main/java/com/tpourjalali/farmsafe/LanguageFragment.java
+++ FarmSafe/app/src/main/java/com/tpourjalali/farmsafe/LanguageFragment.java
@@ -1,5 +1,6 @@
 package com.tpourjalali.farmsafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import java.util.ArrayList;
 import java.util.List;
 import android.content.Context;
+import android.widget.TextView;
 
 /**
  * Created by micah on 1/13/18.
@@ -45,7 +47,7 @@ public class LanguageFragment extends Fragment {
         languageList.add(new LanguageRow("日本 (Japanese)","jp"));
 
         mRecyclerView = V.findViewById(R.id.language_recycler);
-        //mAdapter = new LanguageAdapter(languageList, new LanguageAdapter.OnItemClickListener() );
+        mAdapter = new LanguageAdapter(languageList);
         mLayoutManager = new LinearLayoutManager(getActivity());
 
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
@@ -57,4 +59,52 @@ public class LanguageFragment extends Fragment {
 
         return V;
     }
+    private class LanguageViewHolder extends RecyclerView.ViewHolder {
+        public TextView lang;
+
+        public LanguageViewHolder(View view) {
+            super(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getActivity(), HomeActivity.class);
+                    startActivity(i);
+                }
+            });
+            lang = (TextView) view.findViewById(R.id.lang);
+
+        }
+
+        public void bind(final LanguageRow row){
+            lang.setText(row.getRowString());
+        }
+    }
+    private class LanguageAdapter extends RecyclerView.Adapter<LanguageViewHolder> {
+        private List<LanguageRow> languageList;
+
+        public LanguageAdapter(List<LanguageRow> languageList) {
+            this.languageList = languageList;
+        }
+
+        @Override
+        public LanguageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.language_row, parent, false);
+            return new LanguageViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(LanguageViewHolder holder, int position) {
+            LanguageRow row = languageList.get(position);
+            holder.lang.setText(row.getRowString());
+            holder.bind(row);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return languageList.size();
+        }
+    }
+
 }
