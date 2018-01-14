72d63386efc7f01247896574dd00acc7b331e6aa
matthew-schultz <mschultz@hawaii.edu>
2018-01-13 23:13:58 -1000

    update ModelCourse class
    create getContentValues() to anticipate data retrieval from courses db
    
    import ContentValues and CourseDbSchema
    
    create private variables for contents of db

--- FarmSafe/app/src/main/java/com/tpourjalali/farmsafe/ModelCourse.java
+++ FarmSafe/app/src/main/java/com/tpourjalali/farmsafe/ModelCourse.java
@@ -1,10 +1,12 @@
 package com.tpourjalali.farmsafe;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.tpourjalali.farmsafe.database.CourseBaseHelper;
+import com.tpourjalali.farmsafe.database.CourseDbSchema;
 
 import java.util.ArrayList;
 
@@ -16,12 +18,40 @@ public class ModelCourse {
 //    private static
     private Context mContext;
     private SQLiteDatabase mDatabase;
-    private List<Course> mCourses;
+    private String courseLanguage;
+    private String courseId;
+    private String courseName;
+//    private List<Course> mCourses;
+
+//    public static ModelCourse get(Context context) {
+//
+//    }
 
     private ModelCourse(Context context) {
         mContext = context.getApplicationContext();
         mDatabase = new CourseBaseHelper(mContext)
                 .getWritableDatabase();
 //        mCourses = new ArrayList<>();
+    }
+
+    public String getCourseLanguage() {
+        return courseLanguage;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    private ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(CourseDbSchema.CourseTable.Cols.COURSE_ID, courseId);
+        values.put(CourseDbSchema.CourseTable.Cols.LANGUAGE, courseLanguage);
+        values.put(CourseDbSchema.CourseTable.Cols.COURSE_NAME, courseName);
+
+        return values;
     }
 }
